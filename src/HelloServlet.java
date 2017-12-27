2bf889c1cdc97f641554db728b48305ff4677eb7
kagowtham <ka.gow1996@gmail.com>
2017-12-27 15:30:11 +0530

    Initial commit

---
+++ src/HelloServlet.java
@@ -0,0 +1,56 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet {
+	String a="on";
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		try {
+		  
+		 
+         String recievedString =request.getParameter("status");
+         System.out.println("recived "+recievedString);
+         if(recievedString.trim().equals("on")||recievedString.trim().equals("off")) {
+        	a=recievedString.trim();
+        	System.out.println("server: changed state "+recievedString);
+         }
+		 
+		 response.getWriter().append(a);
+		}catch(Exception e) {
+			 response.getWriter().append(a);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
